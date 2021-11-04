fbb1e7664fdbf14d501b7321bc3e1aada7683032
Krushna Prasad Sahoo <64131038+Krushna-Prasad-Sahoo@users.noreply.github.com>
2021-11-04 09:56:41 +0530

    path provided in desc.
    C:\Users\KIIT\Documents\java_code\PracticeMyself\src\com\java\practice\oop1

---
+++ Fish.java
@@ -0,0 +1,18 @@
+package com.java.practice.oop1;
+
+public class Fish extends Animal{
+	
+	public Fish(String name) {
+		super(name);
+	}
+
+	public void Swim() {
+		System.out.println("..swimming...");
+	}
+
+	public void movement() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
