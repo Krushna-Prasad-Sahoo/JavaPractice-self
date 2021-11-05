459497d89e831ebb1f45f49b61094395fb6f5c3d
Krushna Prasad Sahoo <64131038+Krushna-Prasad-Sahoo@users.noreply.github.com>
2021-11-05 19:15:04 +0530

    path provided in desc.
    C:\Users\KIIT\Documents\java_code\PracticeMyself\src\com\java\practice\ControlFlow

---
+++ Pattern3.java
@@ -0,0 +1,16 @@
+package com.java.practice.ControlFlow;
+
+public class Pattern3 {
+	public static void main(String[] args) {
+
+		for (int j = 6; j > 0; j--) {
+			for (int i = j; i >= 1; i--) {
+
+				System.out.print(" x ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
