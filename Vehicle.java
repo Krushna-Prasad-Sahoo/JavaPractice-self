c5778ffcb1a089bc9bd956b543406ecbb3dbddeb
Krushna Prasad Sahoo <64131038+Krushna-Prasad-Sahoo@users.noreply.github.com>
2021-11-21 22:50:57 +0530

    path provided in desc.
    C:\Users\KIIT\Documents\java_code\MyPracticeProjects\src\com\project\CarDealership

---
+++ Vehicle.java
@@ -0,0 +1,22 @@
+package com.project.CarDealership;
+
+public class Vehicle {
+
+	String carName;
+	String carModel;
+	double carPrice;
+	int noOfSeat;
+	int carSpeed;
+	
+	public Vehicle(String carName, String carModel, double carPrice, int noOfSeat, int carSpeed) {
+		super();
+		this.carName = carName;
+		this.carModel = carModel;
+		this.carPrice = carPrice;
+		this.noOfSeat = noOfSeat;
+		this.carSpeed = carSpeed;
+	}	
+	
+	
+	
+}
