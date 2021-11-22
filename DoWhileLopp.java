d19ba1a86ef85ff5c437dda7413adfcd03ad8657
Krushna Prasad Sahoo <64131038+Krushna-Prasad-Sahoo@users.noreply.github.com>
2021-11-22 22:18:52 +0530

    path provided in desc.
    C:\Users\KIIT\Documents\java_code\PracticeMyself\src\com\java\practice\ControlFlow

---
+++ DoWhileLopp.java
@@ -0,0 +1,14 @@
+package com.java.practice.ControlFlow;
+
+public class DoWhileLopp {
+	public static void main(String[] args) {
+		
+		int i = 1;
+		
+		do {
+			System.out.println("Loop started now ..");
+		}
+		while (i < 1);
+	}
+
+}
